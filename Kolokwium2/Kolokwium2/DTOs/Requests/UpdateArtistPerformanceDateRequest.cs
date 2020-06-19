c0acfc5f5dda3a021ff2b4eaf51cc07b93326a80
Jan Rejnowski <jan.rejnowski@gmail.com>
2020-06-19 14:33:03 +0200

    Początek walidowania requesta do 2 zadania

--- Kolokwium2/Kolokwium2/DTOs/Requests/UpdateArtistPerformanceDateRequest.cs
+++ Kolokwium2/Kolokwium2/DTOs/Requests/UpdateArtistPerformanceDateRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace Kolokwium2.DTOs.Requests
 {
     public class UpdateArtistPerformanceDateRequest
     {
+        [Required]
         public int idArtist { get; set; }
+        [Required]
         public int idEvent { get; set; }
+        [Required]
         public DateTime performanceDate { get; set; }
     }
 }
